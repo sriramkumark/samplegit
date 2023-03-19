3eb0e4cec0142a9bf94593efc3aa0e6907ad73e5
sriramdk <sriramkumar.dk@gmail.com>
2023-03-19 11:57:41 +0530

    commit by sampleproject-x person

--- src/testdemopack/sampleclass.java
+++ src/testdemopack/sampleclass.java
@@ -5,6 +5,7 @@ public class sampleclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("welcome to demo class");
+     System.out.println("changes done in java project");
 	}
 
 }
